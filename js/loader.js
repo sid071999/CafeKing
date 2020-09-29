11557c5bcf2e90ea2591e94e02b190e411a0046f
Sid Jain <sidjain.sb@gmail.com>
2020-09-29 19:51:22 +0530

    Initial Commit

---
+++ js/loader.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.modal').modal();
+    $(".parallax").parallax();
+    $('.sidenav').sidenav();
+    $(".slider").slider({
+        full_width: true
+    });
+    $(".myreviews").carousel({
+        numVisible: 7,
+        shift: 55,
+        padding: 55
+    })
+})
+
+function toggleModal(){
+    var instance = M.Modal.getInstance($('#modal3'))
+    instance.open();
+}
